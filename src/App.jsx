3c41c1b38da0a1276abceaac6139ed1f169e6459
Camilobarondev <camilobarondev@gmail.com>
2024-02-17 17:26:27 -0500

    Se creo el tablero con 9 cuadrados en el componente app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function Sqyare() {
+  return (
+    <>
+    <div>
+      <button>X</button>
+      <button>X</button>
+      <button>X</button>
+    </div>
+    <div>
+      <button>X</button>
+      <button>X</button>
+      <button>X</button>
+    </div>
+    <div>
+      <button>X</button>
+      <button>X</button>
+      <button>X</button>
+    </div>
+    </>)
+}
