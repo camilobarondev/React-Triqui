87b38efcea8956c849a0920ecdf31757b6d1e061
camilobarondev <camilobarondev@gmail.com>
2024-02-17 21:24:00 -0500

    Se agrega funcioalidad para determinar a que jugador le corresponde el turno X|O

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from "react";
 
-function Square(){
-  const [value, setValue] = useState(null);
-  function handleClick(){
-    setValue('X');
-  }
+function Square({ value, onSquareClick }){
+
   return (
-    <button
-      onClick={handleClick}
-    >
+    <button onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquare = squares.slice();
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+    setSquares(nextSquare);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
     <div>
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    </div>
+    <div className=""></div>
+    <div>
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div>
-      <Square/>
-      <Square />
-      <Square />
-    </div>
-    <div>
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
     </>)
 }
