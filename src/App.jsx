a5d7edf5cf7c3b14f36af4098fd92707cc9208ca
camilobarondev <camilobarondev@gmail.com>
2024-02-17 22:10:20 -0500

    Se agrega funcion para determinar el ganador y para validar que no se reescriban los valores de los cuadros que ya tienen un valor

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+    return;
+  }
     const nextSquare = squares.slice();
     if (xIsNext) {
       nextSquare[i] = "X";
@@ -41,4 +44,24 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
     </>)
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 8, 8],
+    [0, 3, 6],
+    [1, 4, 8],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const[a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
